2fb43a5f57360f2397993a75906dc201f3dbc5de
Leonid <kissmykrabeskill@gmail.com>
2022-10-26 23:24:09 +0500

    add jfx

---
+++ client/src/main/java/com/example/client/Controller.java
@@ -0,0 +1,14 @@
+package com.example.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Test to JavaFX Application!");
+    }
+}
---
+++ client/src/main/java/com/example/client/MainFXApp.java
@@ -0,0 +1,23 @@
+package com.example.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFXApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(MainFXApp.class.getResource("sample.fxml"));
+        Scene scene = new Scene(loader.load(), 320, 320);
+        stage.setTitle("Cloud");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
