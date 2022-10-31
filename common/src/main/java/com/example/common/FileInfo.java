ed9c55a71f5957f3fca61f92a443fb3b7e5d95bc
Leonid <kissmykrabeskill@gmail.com>
2022-10-31 22:50:00 +0500

    Send files list

--- common/src/main/java/com/example/common/FileInfo.java
+++ common/src/main/java/com/example/common/FileInfo.java
@@ -1,12 +1,13 @@
 package com.example.common;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-public class FileInfo {
+public class FileInfo implements Serializable {
 
     public enum FileType{
         FILE("F"), DIRECTORY("D");
