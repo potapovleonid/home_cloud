9d742c3aff2e61798f6daafeb92288253d2435d6
Leonid <kissmykrabeskill@gmail.com>
2023-02-07 22:39:37 +0500

    refactor

--- server/src/main/java/com/example/server/network/SQLConnection.java
+++ server/src/main/java/com/example/server/network/SQLConnection.java
@@ -42,33 +42,37 @@ public class SQLConnection {
 
     public static boolean authorizeUser(String login, String password){
         try {
-            String sql = String.format("SELECT login, password FROM users WHERE login ='%s' and password ='%s'", login, password);
-            ResultSet rs = statement.executeQuery(sql);
-            return rs.next();
+            return isFindUser(login, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-    public static boolean changePassword(String login, String oldPassword, String newPassword){
+    public static synchronized boolean changePassword(String login, String oldPassword, String newPassword){
         try {
-            String sql = String.format("SELECT login, password " +
-                                        "FROM users " +
-                                        "WHERE login = '%s' and password = '%s'", login, oldPassword);
-            ResultSet rs = statement.executeQuery(sql);
-            boolean result = rs.next();
+            boolean result = isFindUser(login, oldPassword);
+            String sql;
             if (result){
                 sql = String.format("UPDATE users " +
                                     "SET login = '%s', password = '%s' " +
                                     "WHERE login = '%s', and password ='%s'", login, newPassword, login, oldPassword);
                 statement.executeQuery(sql);
+                return true;
             }
             return false;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
+    }
+
+    private static boolean isFindUser(String login, String oldPassword) throws SQLException {
+        String sql = String.format("SELECT login, password " +
+                                    "FROM users " +
+                                    "WHERE login = '%s' and password = '%s'", login, oldPassword);
+        ResultSet rs = statement.executeQuery(sql);
+        return rs.next();
     }
 
     public static boolean deleteUser(String login, String password){
