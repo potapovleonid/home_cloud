7caedf354aeabf1d064ad8984c94b755b2e4df1b
Leonid <kissmykrabeskill@gmail.com>
2023-03-29 10:55:35 +0500

    Added action on button change pass and check empty fields

--- client/src/main/java/com/example/client/fx/ChangePasswordController.java
+++ client/src/main/java/com/example/client/fx/ChangePasswordController.java
@@ -1,8 +1,15 @@
 package com.example.client.fx;
 
+import com.example.client.network.Network;
+import com.example.client.network.networking.RequestChangePassword;
+import com.example.client.network.networking.RequestList;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.util.Objects;
 
 public class ChangePasswordController {
     @FXML
@@ -11,8 +18,17 @@ public class ChangePasswordController {
     public TextField newPassword;
 
     public void btnChangePassword(ActionEvent actionEvent) {
+        if (Objects.equals(oldPassword.getText(), "") || Objects.equals(newPassword.getText(), "")){
+//            TODO swap all alert
+            JOptionPane.showMessageDialog(null, "Someone field is empty, please try again");
+            return;
+        }
+//        TODO check length, check equals new and old passwords
+        Platform.runLater(() -> Network.getNetwork().getChannel().writeAndFlush(new RequestChangePassword(oldPassword.getText(), newPassword.getText())));
     }
 
     public void btnCancelChangePassword(ActionEvent actionEvent) {
+        ReplaceSceneManager.replaceSceneContent("main.fxml", "Home cloud");
+        Platform.runLater(() -> Network.getNetwork().getChannel().writeAndFlush(new RequestList()));
     }
 }
